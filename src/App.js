717b6bc80c072f53d83c230ddcecacdd90475bd2
“ALEX <alexgtr84@gmail.com>
2019-01-21 16:48:07 +0100

    Saving the last version

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 import Title from './components/Title'
+import LightSwitch from './components/LightSwitch'
+// import User from './components/User';
+import UserBoard from './components/UserBoard';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
 
         <main>
-          <Title />
+          <Title content="The best app in the history of apps"/>
+          <LightSwitch />
+          <UserBoard />
         </main>
       </div>
     );
